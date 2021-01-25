169933a6db03f7f24698f4631aace0e2bfd9ee08
Anil Degala <anilkumar.degala@childrens.harvard.edu>
2021-01-25 11:45:44 -0500

    Update install-dependencies.sh

--- initial-configuration/install-dependencies.sh
+++ initial-configuration/install-dependencies.sh
@@ -17,13 +17,13 @@ echo "Finished docker install, enabling and starting docker service"
 systemctl enable docker
 service docker start
 
-yum -y update
-yum install -y python3
-curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-mount /tmp -o remount,exec
-docker-compose --version
+#yum -y update
+#yum install -y python3
+#curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#chmod +x /usr/local/bin/docker-compose
+#mount /tmp -o remount,exec
+#docker-compose --version
 
-../jenkins/install-jenkins.sh
+#../jenkins/install-jenkins.sh
 
 echo "Installation script complete."
