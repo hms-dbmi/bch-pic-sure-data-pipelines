dea1b23090e63db114b3f9c1687edc97f31450ec
anilk2hms <anilkumar.degala@childrens.harvard.edu>
2021-02-23 08:54:53 -0500

    minor fixes

--- initial-configuration/install-dependencies.sh
+++ initial-configuration/install-dependencies.sh
@@ -3,27 +3,29 @@
 CWD=`pwd`
 
 echo "Starting update"
-yum -y update 
+sudo yum -y update 
 
 echo "Finished update, adding epel, docker-ce  repositories and installing wget and yum-utils"
-yum -y install epel-release wget yum-utils
-yum-config-manager  --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum -y update
+sudo yum -y install epel-release wget yum-utils
+sudo yum-config-manager  --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum -y update
 
 echo "Added docker-ce repo, starting docker install"
-yum -y install docker-ce docker-ce-cli containerd.io
+sudo yum -y install docker-ce docker-ce-cli containerd.io
 
 echo "Finished docker install, enabling and starting docker service"
-systemctl enable docker
-service docker start
+sudo systemctl enable docker
+sudo service docker start
 
-#yum -y update
-#yum install -y python3
-#curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#chmod +x /usr/local/bin/docker-compose
-#mount /tmp -o remount,exec
-#docker-compose --version
+sudo yum -y update
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo mount /tmp -o remount,exec
+sudo /usr/local/bin/docker-compose --version
 
-#../jenkins/install-jenkins.sh
+sudo chmod a+x ../jenkins/install-jenkins.sh
+cd ../jenkins
+sudo sh ./install-jenkins.sh
+cd ../initial-configuration
 
 echo "Installation script complete."
--- jenkins/install-jenkins.sh
+++ jenkins/install-jenkins.sh
@@ -7,15 +7,15 @@ JENKINS=`pwd`
 echo 'JENKINS dir: ' $JENKINS
 
 cd $JENKINS
-/usr/local/bin/docker-compose down
-/usr/local/bin/docker-compose build && /usr/local/bin/docker-compose up -d
+sudo /usr/local/bin/docker-compose down
+sudo /usr/local/bin/docker-compose build && /usr/local/bin/docker-compose up -d
 
 sleep 10
 
-docker stop jenkins
+sudo docker stop jenkins
 sleep 5
-cp config.xml /var/jenkins_home
-cp -R ./jobs /var/jenkins_home/
+sudo cp config.xml /var/jenkins_home
+sudo cp -R ./jobs /var/jenkins_home/
 
-docker start jenkins
+sudo docker start jenkins
 sleep 10
